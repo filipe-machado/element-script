7d52374a9a7f2be1c071dfe7c6b1b4aec2693947
filipe-machado <ax-filipe@hotmail.com>
2019-04-08 16:18:19 -0500

    Adding SVG to background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 import Main from './components/Main'
+import $ from 'jquery'
 
 class App extends Component {
+  componentDidMount() {
+    $(window).on('load', () => {
+      $('.inner').delay(500).fadeOut('slow')
+      setTimeout(() => {
+        $('body').css('overflow', 'visible')
+      }, 500)
+    })
+  }
   render() {
     let component = ''
     switch ( this.props.location ) {
@@ -14,7 +23,9 @@ class App extends Component {
         break
     }
     return (
-      <div className="App">        
+      <div className="App">
+        <div className="inner">
+        </div>
         { component }
       </div>
     );
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
+import M from 'materialize-css'
 
 export default class Services extends Component {
+    componentDidMount() {
+        document.addEventListener('DOMContentLoaded', function() {
+            const elems = document.querySelectorAll('.carousel');
+            M.Carousel.init(elems, {
+                indicators: true,
+                duration: 200
+            });
+        });     
+    }
+    
     render() {
+
         return(
             <div className="container box--second">
                 <h2 id="services">serviços</h2>
+
+                <div className="carousel carousel-slider center">
+                    <div className="carousel-fixed-item center">
+                        
+                    </div>
+                    <div className="carousel-item white" href="#one!">
+                        <h6 className="orange-text">First Panel</h6>
+                        <p className="orange-text">This is your first panel</p>
+                    </div>
+                    <div className="carousel-item white" href="#two!">
+                        <h6 className="orange-text">Second Panel</h6>
+                        <p className="orange-text">This is your second panel</p>
+                    </div>
+                    <div className="carousel-item white" href="#three!">
+                        <h6 className="orange-text">Third Panel</h6>
+                        <p className="orange-text">This is your third panel</p>
+                    </div>
+                    <div className="carousel-item white" href="#four!">
+                        <h6 className="orange-text">Fourth Panel</h6>
+                        <p className="orange-text">This is your fourth panel</p>
+                    </div>
+                </div>
             </div>
         )
     }
